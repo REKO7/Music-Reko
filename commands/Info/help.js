a681363a71c30c5ac79612c7c75f4ba712d5487d
REKO7 <71840491+REKO7@users.noreply.github.com>
2021-09-30 02:53:40 +0300

    Update help.js

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -45,9 +45,9 @@ module.exports = {
         const embed = new MessageEmbed()
           .setColor(ee.color)
           .setThumbnail(ee.footericon)
-          .setTitle("HELP MENU 🔰 Commands")
+          .setTitle("Music Bot✨ Commands")
           .setDescription(`**[Invite me with __Slash Commands__ Permissions](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands), cause all of my Commands are available as Slash Commands too!**\n\n> Check out the [**Dashboard**](${websiteSettings.website.domain}/dashboard/${message.guild.id}) or the [**Live Music Queue**](${websiteSettings.website.domain}/queue/${message.guild.id})`)
-          .setFooter(`To see Command Description & Information, type: ${prefix}help [CMD NAME]`, ee.footericon);
+          .setFooter(`Music Bot✨ ,type: ${prefix}help `, ee.footericon);
         const commands = (category) => {
           return client.commands.filter((cmd) => cmd.category === category).map((cmd) => `\`${cmd.name}\``);
         };
